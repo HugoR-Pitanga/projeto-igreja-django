07bb00a974bbfafcf369d5a0aafa943f1e28d4d7
HugoR-Pitanga <romeu_hg@hotmail.com>
2021-05-15 15:59:21 -0300

    criado a parte financeira

--- igreja/admin.py
+++ igreja/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Ministerio
+from .models import Ministerio, Agenda, MovimentoFinanceiro, TipoMovimentoFinanceiro, OrigemMovimentoFinanceiro
 
 admin.site.register(Ministerio)
-
+admin.site.register(Agenda)
+admin.site.register(MovimentoFinanceiro)
+admin.site.register(TipoMovimentoFinanceiro)
+admin.site.register(OrigemMovimentoFinanceiro)
--- igreja/models.py
+++ igreja/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.fields import CharField
 from django.utils import timezone
 
 class Ministerio(models.Model):
@@ -12,3 +13,33 @@ class Ministerio(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Agenda(models.Model):
+    nome = models.CharField(max_length=300)
+    data = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+class OrigemMovimentoFinanceiro(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+
+class TipoMovimentoFinanceiro(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+
+class MovimentoFinanceiro(models.Model):
+    tipo = models.ForeignKey(TipoMovimentoFinanceiro, on_delete=models.CASCADE)
+    origem = models.ForeignKey(OrigemMovimentoFinanceiro, on_delete=models.CASCADE)
+    data_movimento = models.DateField()
+    valor = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.data_movimento
